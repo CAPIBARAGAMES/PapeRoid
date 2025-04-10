4d57b1c5f03241616497fd77dbab60d8cdaabf83
Luis <losjuegosdelcapibara@gmail.com>
2025-04-09 23:49:44 -0600

    Se agrego el conrolador basico del jugador

--- scenes/star_ship.gd
+++ scenes/star_ship.gd
@@ -1,24 +1,23 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var speed = Global.speed
+var angular_speed = Global.angular_speed * 1.5
 
 
-func _physics_process(delta):
-	# Add the gravity.
+func _process(delta):
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
 
+	rotation += angular_speed * direction * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.y = direction * SPEED
-	else:
-		velocity.y = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		$AnimatedSprite2D.play("fiun")
+		velocity = Vector2.UP.rotated(rotation) * speed
+	else :
+		$AnimatedSprite2D.play("statik")
+	position += velocity * delta
